969977802db7f9d6909e1eb3ffb4de938274d07d
DLehenbauer <DLehenbauer@users.noreply.github.com>
2016-09-16 11:29:43 -0700

    'set-cookie.js' must exist for 'wptserve' to process '.sub.headers'

---
+++ content-security-policy/blink-contrib/resources/set-cookie.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// This file's only purpose is to be included via <script> so that wptserve will notice
+// 'set-cookie.js.sub.headers' and set the cookie via the HTTP response headers.
